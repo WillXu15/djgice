2cca683d02da3f35221229d87e90df67f6be0fd9
Will Xu <williamxu15@gmail.com>
2015-03-05 02:55:16 -0500

    audio playing working

--- dj.py
+++ dj.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python
-import subprocess, fileinput
+import subprocess
+import glob
+import os
+import pygame
+import time
 
-songlist = []
+songs = ["moSFlvxnbgk.wav"]
+pygame.mixer.init(22050, -16, 2, 2048)
 
-url = "https://www.youtube.com/watch?v=moSFlvxnbgk"
+def get_song(url):
+    print "DOWNLOADING SONG"
+    subprocess.call("youtube-dl -o '%(id)s.%(ext)s' -x --audio-format mp3 " +url, shell=True)
+    load_song()
 
-subprocess.call()
+def load_song():
+    print "LOADING SONG"
+    song = max(glob.iglob('*.mp3'), key=os.path.getctime)
+    process = "ffmpeg -i "+song+" "+song[:-4]+".wav"
+    print process
+    subprocess.call(process, shell=True)
+    songs.append(song[:-4]+".wav")
+
+def main():
+#    get_song("https://www.youtube.com/watch?v=moSFlvxnbgk")
+    while True:
+        pygame.mixer.music.load(songs[0])
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            time.sleep(15)
+
+if __name__ == "__main__": main()
